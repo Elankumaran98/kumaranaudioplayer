08984f3727d99512b8896532cb3faab28292fbb9
Elankumaran98 <siva98kumarane@gmail.com>
2021-07-14 22:22:57 +0530

    MyTapp class created

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:kumaranaudioplayer/Pages/my_tabs.dart';
 import 'package:kumaranaudioplayer/Widgets/app_colors.dart' as AppColors;
 
 
@@ -11,8 +12,10 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
   late List popularAlbums;
+  late ScrollController _scrollController;
+  late TabController _tabController;
 
   ReadData() async {
      await  DefaultAssetBundle.of(context).loadString(
@@ -20,6 +23,8 @@ class _HomePageState extends State<HomePage> {
     then((s){
       setState(() {
         popularAlbums= json.decode(s);
+        _tabController=TabController(length: 3, vsync: this);
+        _scrollController=ScrollController();
       });
     } );
   }
@@ -90,6 +95,71 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            Expanded(child: NestedScrollView(
+                controller: _scrollController,
+                headerSliverBuilder: (BuildContext context,bool isScroll){
+                  return [
+                    SliverAppBar(
+                      pinned: true,
+                        backgroundColor:AppColors.silverBackground,
+                      bottom: PreferredSize(
+                        preferredSize: Size.fromHeight(50),
+                        child:Container(
+                          margin: const EdgeInsets.all(0),
+                          child: TabBar(
+                              indicatorPadding: const EdgeInsets.only(bottom:20 ),
+                              indicatorSize:TabBarIndicatorSize.label ,
+                              labelPadding: const EdgeInsets.only(right: 10),
+                              controller: _tabController,
+                              isScrollable: true,
+                              indicator: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.2),
+                                    blurRadius: 7,
+                                    offset: Offset(0,0)
+                                  )
+                                ]
+                              ),
+                              tabs: [
+                                MyTabs(color: AppColors.menu1Color, text: "New"),
+                                MyTabs(color: AppColors.menu2Color, text: "Popular"),
+                                MyTabs(color: AppColors.menu3Color, text: "Trending"),
+                              ]),
+                        ) ,
+                      ),
+                    )
+                  ];
+                },
+                body: TabBarView(
+                  controller: _tabController,
+                    children: [
+                  Material(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                      ),
+                      title: Text("Content"),
+                    ),
+                  ),
+                  Material(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                      ),
+                      title: Text("Content"),
+                    ),
+                  ),
+                  Material(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                      ),
+                      title: Text("Content"),
+                    ),
+                  ),
+                ])))
 
           ],
         ),
